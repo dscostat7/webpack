699e97a9d35bf02675810640fd579cbabdf70571
Diego_Souza <diego.s.costa@fiesc.com.br>
2021-05-06 19:39:42 -0300

    utilizando webpack para implementar o carregamento preguiçoso, carregando o metodo, apenas quando chamado

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,14 +3,31 @@ const babiliPlugin = require('babili-webpack-plugin');
 const extractTextPlugin = require('extract-text-webpack-plugin');
 const optimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const webpack = require('webpack');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
 let plugins = [];
+
+plugins.push(new htmlWebpackPlugin({
+    hash: true,
+    minify: {
+        html5: true,
+        collapseWhitespace: true,
+        removeComments: true
+    },
+    filename: 'index.html',
+    template: __dirname + '/main.html'
+}));
 
 plugins.push(new extractTextPlugin('styles.css'));
 
 plugins.push(new webpack.ProvidePlugin({
     '$': 'jquery/dist/jquery.js',
     'jQuery': 'jquery/dist/jquery.js'
+}));
+
+plugins.push(new webpack.optimize.CommonsChunkPlugin({
+    name: 'vendor',
+    filename: 'vendor.bindle.js'
 }));
 
 if (process.env.NODE_ENV == 'production') {
@@ -31,11 +48,13 @@ if (process.env.NODE_ENV == 'production') {
 }
 
 module.exports = {
-    entry: './app-src/app.js',
+    entry: {
+        app: './app-src/app.js',
+        vendor: ['jquery', 'bootstrap', 'reflect-metadata']
+    },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: 'dist'
+        path: path.resolve(__dirname, 'dist')
     },
     module: {
         rules: [
